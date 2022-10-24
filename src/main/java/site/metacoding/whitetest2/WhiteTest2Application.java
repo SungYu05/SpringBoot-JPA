25dc51165052f14e7589896768155303569ad66e
SungYu05 <tjddb0531@naver.com>
2022-10-24 20:58:33 +0900

    스프링부트 JPA 초기세팅

---
+++ src/main/java/site/metacoding/whitetest2/WhiteTest2Application.java
@@ -0,0 +1,13 @@
+package site.metacoding.whitetest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/whitetest2/WhiteTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.whitetest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
